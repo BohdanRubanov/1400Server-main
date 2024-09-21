9a49b5a5f0a4f10a91ef7f731ec5c322976196dd
Bohdan_Rubanov <bgbg78915@gmail.com>
2024-09-21 19:27:54 +0300

    final_w

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 //підключає бібліотеку експрес
 const express = require('express')
 const moment = require('moment')
+const path = require('path')
 //створення експерес застосунку
 const app = express()
 //вказівник на застосунок або сервіс запущений на комп'ютері
 const PORT = 8000
 //адреса на наш комп'ютер
 const HOST = 'localhost'
+
+app.use('/static/', express.static(path.join(__dirname, 'static'))) 
+
 //оброблення get запиту, першим аргументом посилання, другим функція на цей запит
 //ця функція відправляє відповідь
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./templates/index.html"))
     console.log("ktoto zashel na stranicu")
 })
 app.get('/date', (req, res) => {
